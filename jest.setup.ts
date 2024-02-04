08cfd9792a531647218befe1a80ed2a5b7ad1be5
jin <bnt10@naver.com>
2024-02-04 13:12:29 +0900

    [TC-3] feat: initialize project with essential setup
     초기 프로젝트 구조 및 설정 파일 구성
    
    - Next.js: 프론트엔드 React 프레임워크로 동적 웹 애플리케이션 구축의 기반.
    - React Query: 서버 상태 관리를 위한 도구, 데이터 페칭 및 캐싱 최적화.
    - Recoil: 상태 관리 라이브러리, React 애플리케이션의 전역 상태 관리.
    - Tailwind CSS: 유틸리티-퍼스트 CSS 프레임워크로 커스텀 디자인 적용.
    - Storybook: UI 컴포넌트 개발 및 문서화를 위한 도구.
    - ESLint: 코드 품질 및 스타일 가이드라인을 유지하기 위한 Linting 도구.
    - TypeScript: 정적 타입 지정을 통한 코드 안정성 및 유지보수성 향상.
    - Husky와 lint-staged를 통한 Git 훅 및 코드 스타일 관리 설정.
    - Jest를 사용한 테스팅 환경 구축.

---
+++ jest.setup.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+import '@testing-library/jest-dom'
+
+import failOnConsole from 'jest-fail-on-console'
+
+failOnConsole()
